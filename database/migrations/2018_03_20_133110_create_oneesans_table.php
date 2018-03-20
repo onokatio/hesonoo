65e380f55969bd5ed4eafc932d65849709800f1b
onokatio <onokatio@gmail.com>
2018-03-20 22:32:09 +0900

    Oneesan model

---
+++ database/migrations/2018_03_20_133110_create_oneesans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOneesansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oneesans', function (Blueprint $table) {
+            $table->increments('id');
+						$table->integer('userid');
+						$table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oneesans');
+    }
+}
