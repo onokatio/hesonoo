dbce3625a55f4a1aebd07e06a21c60213835a627
onokatio <onokatio@gmail.com>
2018-03-20 21:56:45 +0900

    update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,8 +71,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">ログイン</a>
+                        <a href="{{ route('register') }}">登録</a>
                     @endauth
                 </div>
             @endif
